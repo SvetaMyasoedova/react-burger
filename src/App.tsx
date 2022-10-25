4b2d006ffcb1268ab5828e680559372ddb804427
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-10-25 12:36:00 +0600

    BurgerIngredients was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AppHeader from './components/app-header/AppHeader';
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import AppHeader from "./components/app-header/AppHeader";
+import BurgerIngredients from "./components/burger-ingredients/BurgerIngredients";
 
 function App() {
   return (
     <div className="App">
-      <AppHeader/>
+      <AppHeader />
+      <BurgerIngredients />
     </div>
   );
 }
---
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Tabs from "../tab-ingredients/Tabs";
+
+
+function BurgerIngredients() {
+
+	return (
+		<Tabs></Tabs>
+	)
+}
+
+export default BurgerIngredients;
