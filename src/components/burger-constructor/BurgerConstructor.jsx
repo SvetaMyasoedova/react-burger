2eca3d79c8968a443d84571930df5014920d3fb4
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-10-27 17:43:27 +0600

    BurgerConstructor was fixed

--- src/components/burger-constructor/BurgerConstructor.jsx
+++ src/components/burger-constructor/BurgerConstructor.jsx
@@ -9,20 +9,39 @@ import { data } from "../../utils/data";
 
 function BurgerConstructor() {
   return (
-    <div
-      className={`${stylesCunstructor.scroll} mt-2`}
-      style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+    <section
+      className={`${stylesCunstructor.burgerConstructor} mt-2`}
+      style={{ display: "flex", flexDirection: "column", gap: "10px"}}
     >
-      {data.map((ingredient) => (
-        <ConstructorElement
-          key={ingredient._id}
-          type="top"
-          isLocked={true}
-          text={ingredient.name}
-          price={ingredient.price}
-          thumbnail={ingredient.image}
-        />
-      ))}
+      <ConstructorElement
+        key={data[0]._id}
+        type="top"
+        isLocked={true}
+        text={data[0].name}
+        price={data[0].price}
+        thumbnail={data[0].image}
+      />
+      <div className={`${stylesCunstructor.scroll} mb-2`}>
+        {data.map((ingredient) => (
+          <ConstructorElement
+            key={ingredient._id}
+            type="top"
+            isLocked={false}
+            text={ingredient.name}
+            price={ingredient.price}
+            thumbnail={ingredient.image}
+          />
+        ))}
+      </div>
+
+      <ConstructorElement
+        key={data[data.length - 1]._id}
+        type="top"
+        isLocked={true}
+        text={data[data.length - 1].name}
+        price={data[data.length - 1].price}
+        thumbnail={data[data.length - 1].image}
+      />
 
       <div className={`${stylesCunstructor.order} mt-10 mb-20`}>
         <p className="mr-2 text text_type_digits-medium">610</p>
@@ -34,7 +53,7 @@ function BurgerConstructor() {
           Оформить заказ
         </Button>
       </div>
-    </div>
+    </section>
   );
 }
 
--- src/components/element-header/ElementHeader.jsx
+++ src/components/element-header/ElementHeader.jsx
@@ -2,7 +2,7 @@ import stylesElement from "./element-header.module.css";
 
 function ElementHeader({ icon, text }) {
   return (
-    <div className={stylesElement.main}>
+    <div className={`${stylesElement.main} p-2`}>
       <div>{icon}</div>
       <div>{text}</div>
     </div>
