51ccd98673e4fcd0be8f088894b69ed427cea725
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-11-03 21:38:34 +0600

    close Modal using Esc key on keyboard was added

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,5 +1,6 @@
-import  ReactDOM from "react-dom";
-import PropTypes from 'prop-types';
+import { useEffect } from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 import ModalHeader from "./modal-header/ModalHeader";
 import styleModal from "./modal.module.css";
 import ModalOverlay from "../modal-overlay/ModalOverlay";
@@ -7,14 +8,22 @@ import ModalOverlay from "../modal-overlay/ModalOverlay";
 const modalRoot = document.getElementById("modal-root");
 
 function Modal({ header, onClose, children }) {
+  useEffect(() => {
+    const close = (e) => {
+      if (e.keyCode === 27) {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", close);
+    return () => window.removeEventListener("keydown", close);
+  }, []);
   return ReactDOM.createPortal(
     <>
       <div className={styleModal.modal}>
-        <ModalHeader header = {header} onClose={onClose}></ModalHeader>
+        <ModalHeader header={header} onClose={onClose}></ModalHeader>
         {children}
       </div>
-		<ModalOverlay onClose={onClose}/>
-      
+      <ModalOverlay onClose={onClose} />
     </>,
     modalRoot
   );
@@ -24,7 +33,6 @@ Modal.propTypes = {
   header: PropTypes.string,
   onClose: PropTypes.func,
   children: PropTypes.element,
-
-}
+};
 
 export default Modal;
