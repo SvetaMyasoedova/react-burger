db644e970915b44d4525b2fbcc7652049b49f98e
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-11-13 17:19:54 +0600

    initial state was added

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,3 +1,10 @@
 import { combineReducers } from "redux";
 
+const initialState = {
+  data: [],
+  constructorIngredients: [],
+  currentIngredient: {},
+  createdOrder: {},
+};
+
 export const rootReducer = combineReducers({});
