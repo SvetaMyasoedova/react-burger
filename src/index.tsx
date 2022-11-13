d6a4c9eaae02b21e0e5236ea111c6e5b91d25335
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-11-13 12:45:00 +0600

    Devtools were connetcted

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,31 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { compose, createStore } from "redux";
 
+import { rootReducer } from "./services/reducers";
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers();
+
+const store = createStore(rootReducer, enhancer);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
