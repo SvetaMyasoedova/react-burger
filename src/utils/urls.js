a167d741f2b55a007796874ff4827ef32aaaf717
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-11-19 16:55:44 +0600

    global vars were renamed to upper case

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,3 @@
-const apiUrl = "https://norma.nomoreparties.space/api";
-export const ingredienstsUrl = apiUrl + "/ingredients";
-export const orderUrl = apiUrl + "/orders";
+const API_URL = "https://norma.nomoreparties.space/api";
+export const INGREDIENTS_URL = API_URL + "/ingredients";
+export const ORDER_URL = API_URL + "/orders";
