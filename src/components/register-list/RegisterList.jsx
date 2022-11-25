1b64b3dd2e4c170b49784bfb72b94c51f0687877
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-11-25 13:31:13 +0600

    handleNewUser was added

--- src/components/register-list/RegisterList.jsx
+++ src/components/register-list/RegisterList.jsx
@@ -1,15 +1,64 @@
+import { useState } from "react";
 import stylesRegister from "./register-list.module.css";
 
 import { Link } from "react-router-dom";
 
 import AppHeader from "../app-header/AppHeader";
-import { Email } from "./email-input/Email";
 import { NameInput } from "./name-input/NameInput";
 import { Password } from "./password-input/Password";
-import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Button, EmailInput } from "@ya.praktikum/react-developer-burger-ui-components";
 
 function RegisterList() {
- 
+
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeUserName = (e) => {
+    setUserName(e.target.value);
+  };
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+
+  const handleNewUser = () => {
+    fetch(" https://norma.nomoreparties.space/api/auth/register", {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        name: userName,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("");
+        } else {
+          
+          return res.json();
+          
+        }
+      })
+      .then((res) => {
+        console.log('NewUser' + res)
+        if (res && res.success) {
+         
+        }
+      });
+  };
   return (
     <div>
       <div className="mb-30">
@@ -20,12 +69,17 @@ function RegisterList() {
         <p className="text text_type_main-medium">Регистрация</p>
       </div>
       <div className={`${stylesRegister.input} mb-5`}>
-        <NameInput placeholder='Имя'/>
-        <Email />
-        <Password />
+        <NameInput placeholder='Имя' onChange={onChangeUserName} value={userName}/>
+         <EmailInput
+        onChange={onChangeEmail}
+        value={email}
+        name={"email"}
+        isIcon={false}
+      />
+        <Password onChange={onChangePassword} value={password}/>
       </div>
       <div className={`${stylesRegister.button} mb-20`}>
-        <Button htmlType="button" type="primary" size="large">
+        <Button onClick={handleNewUser} htmlType="button" type="primary" size="large">
           Зарегистрироваться
         </Button>
       </div>
