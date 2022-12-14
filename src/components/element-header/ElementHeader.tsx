b8d51b941c9f72e65aa6155b725919a57e6b1bc0
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-12-14 16:02:47 +0700

    typescript was added

---
+++ src/components/element-header/ElementHeader.tsx
@@ -0,0 +1,23 @@
+// import PropTypes from 'prop-types';
+import stylesElement from "./element-header.module.css";
+
+interface IHeader {
+  icon: any;
+  text: string;
+}
+
+function ElementHeader({ icon, text }: IHeader) {
+  return (
+    <div className={`${stylesElement.main} p-2`}>
+      <div>{icon}</div>
+      <div>{text}</div>
+    </div>
+  );
+}
+
+// ElementHeader.propTypes = {
+//   icon: PropTypes.element.isRequired,
+//   text: PropTypes.string.isRequired,
+// };
+
+export default ElementHeader;
