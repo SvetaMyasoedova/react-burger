34304094a36ed8ed0d685c341a1597e9d642206a
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-12-15 15:30:56 +0700

    typescript was added

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -5,11 +5,17 @@ import ModalHeader from "./modal-header/ModalHeader";
 import styleModal from "./modal.module.css";
 import ModalOverlay from "../modal-overlay/ModalOverlay";
 
-const modalRoot = document.getElementById("modal-root");
+const modalRoot = document.getElementById("modal-root") as HTMLDivElement;
 
-function Modal({ header, onClose, children }) {
+interface IModal {
+  header: string;
+  onClose: any;
+  children: React.ReactNode; 
+}
+
+function Modal({ header, onClose, children }: IModal) {
   useEffect(() => {
-    const close = (e) => {
+    const close = (e: KeyboardEvent) => {
       if (e.key === 'Escape') {
         onClose();
       }
@@ -29,10 +35,10 @@ function Modal({ header, onClose, children }) {
   );
 }
 
-Modal.propTypes = {
-  header: PropTypes.string.isRequired,
-  onClose: PropTypes.func.isRequired,
-  children: PropTypes.element.isRequired,
-};
+// Modal.propTypes = {
+//   header: PropTypes.string.isRequired,
+//   onClose: PropTypes.func.isRequired,
+//   children: PropTypes.element.isRequired,
+// };
 
 export default Modal;
