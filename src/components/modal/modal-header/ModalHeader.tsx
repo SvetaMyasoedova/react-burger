4bc25f6c1473f2438ff2bed35fc8715365741efa
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-12-15 15:21:34 +0700

    typescript was added

--- src/components/modal/modal-header/ModalHeader.tsx
+++ src/components/modal/modal-header/ModalHeader.tsx
@@ -2,7 +2,12 @@ import PropTypes from "prop-types";
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import styleModalHeader from "./modal-header.module.css";
 
-function ModalHeader({ onClose, header }) {
+type THeader = {
+  onClose: any;
+  header: string;
+}
+
+function ModalHeader({ onClose, header}: THeader) {
   return (
     <div className={`${styleModalHeader.header} pl-10 pr-10 pt-10`}>
       <div className="text text_type_main-large">{header}</div>
@@ -11,9 +16,9 @@ function ModalHeader({ onClose, header }) {
   );
 }
 
-ModalHeader.propTypes = {
-  header: PropTypes.string,
-  onClose: PropTypes.func,
-};
+// ModalHeader.propTypes = {
+//   header: PropTypes.string,
+//   onClose: PropTypes.func,
+// };
 
 export default ModalHeader;
