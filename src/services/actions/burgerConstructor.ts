3134b7e54748bf6038ceff53639b38325e061066
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-12-27 19:49:49 +0700

    typescript was added

--- src/services/actions/burgerConstructor.ts
+++ src/services/actions/burgerConstructor.ts
@@ -3,39 +3,72 @@ import { checkReponse } from "../../utils/refreshToken";
 import { Dispatch } from "redux";
 import { TIngredient } from "../types/data";
 
-export const CONSTRUCTOR_BUN = "CONSTRUCTOR_BUN";
-export const CONSTRUCTOR_MAIN = "CONSTRUCTOR_MAIN";
-export const DELETE_CONSTRUCTOR_INGREDIENT = "DELETE_CONSTRUCTOR_INGREDIENT";
-export const SORTABLE_INGREDIENT = "SORTABLE_INGREDIENT";
+export const CONSTRUCTOR_BUN: "CONSTRUCTOR_BUN" = "CONSTRUCTOR_BUN";
+export const CONSTRUCTOR_MAIN: "CONSTRUCTOR_MAIN" = "CONSTRUCTOR_MAIN";
+export const DELETE_CONSTRUCTOR_INGREDIENT: "DELETE_CONSTRUCTOR_INGREDIENT" =
+  "DELETE_CONSTRUCTOR_INGREDIENT";
+export const SORTABLE_INGREDIENT: "SORTABLE_INGREDIENT" = "SORTABLE_INGREDIENT";
 
 export const GET_ORDER: "GET_ORDER" = "GET_ORDER";
 export const GET_ORDER_FAILED: "GET_ORDER_FAILED" = "GET_ORDER_FAILED";
 export const GET_ORDER_SUCCESS: "GET_ORDER_SUCCESS" = "GET_ORDER_SUCCESS";
 export const CLEAR_ORDER: "CLEAR_ORDER" = "CLEAR_ORDER";
-
 export const CLEAR_CONSTRUCTOR: "CLEAR_CONSTRUCTOR" = "CLEAR_CONSTRUCTOR";
 
+interface actionConstructorBun {
+  readonly type: typeof CONSTRUCTOR_BUN;
+  constructorBun: any;
+}
 
+interface ICount {
+  _id: number;
+}
+interface actionConstructorMain {
+  readonly type: typeof CONSTRUCTOR_MAIN;
+  constructorIngredient: ICount;
+}
+interface actionDeleteElement {
+  uuid: any;
+  id: any;
+  readonly type: typeof DELETE_CONSTRUCTOR_INGREDIENT;
+}
+
+interface actionSortableIngredient {
+  hoverIndex: number;
+  dragIndex: number;
+  readonly type: typeof SORTABLE_INGREDIENT;
+}
+
+interface actionClear {
+  readonly type: typeof CLEAR_CONSTRUCTOR;
+}
 
 interface actionOrderPending {
   readonly type: typeof GET_ORDER;
 }
-
-
 interface actionOrderSuccess {
   readonly type: typeof GET_ORDER_SUCCESS;
   createdOrder: any;
 }
-
 interface actionOrderFail {
   readonly type: typeof GET_ORDER_FAILED;
 }
-
 interface IClearOrder {
   readonly type: typeof CLEAR_ORDER;
 }
 
-export type ActionOrder = actionOrderPending | actionOrderSuccess | actionOrderFail | IClearOrder;
+export type ActionOrder =
+  | actionOrderPending
+  | actionOrderSuccess
+  | actionOrderFail
+  | IClearOrder;
+
+export type ActionIngredient =
+  | actionConstructorBun
+  | actionConstructorMain
+  | actionDeleteElement
+  | actionSortableIngredient
+  | actionClear;
 
 export function getOrder(): any {
   return function (dispatch: Dispatch<ActionOrder>, getState: any) {
