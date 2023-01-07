cbdcb3f29c9fc67f02096d43556eb9bd5f4f9832
SvetaMyasoedova <so.myasoedova@gmail.com>
2023-01-07 10:33:19 +0500

    typescript was added

--- src/services/reducers/registerReducer.ts
+++ src/services/reducers/registerReducer.ts
@@ -1,42 +1,53 @@
-import { GET_REGISTER, GET_REGISTER_FAILED, GET_REGISTER_SUCCESS } from "../actions/register";
+import {
+  GET_REGISTER,
+  GET_REGISTER_FAILED,
+  GET_REGISTER_SUCCESS,
+} from "../actions/register";
 
 import { ActionRegister } from "../actions/register";
 
-const initialState = {
-	registerRequest: false,
-	registerFailed: false,
-	email: "",
-	name: "",
- };
+type TRegisterState = {
+  registerRequest: boolean;
+  registerFailed: boolean;
+  email: string;
+  name: string;
+};
+const initialState: TRegisterState = {
+  registerRequest: false,
+  registerFailed: false,
+  email: "",
+  name: "",
+};
 
- export const registerReducer = (state = initialState, action: ActionRegister) => {
-	switch (action.type) {
-	  case GET_REGISTER: {
-		 return {
-			...state,
-			registerRequest: true,
-			registerFailed: false,
-		 };
-	  }
-	  case GET_REGISTER_SUCCESS: {
-		 return {
-			...state,
-			email: action.email,
-			name: action.name,
-			registerRequest: false,
-		 };
-	  }
-	  case GET_REGISTER_FAILED: {
-		 return {
-			...state,
-			registerFailed: true,
-			registerRequest: false,
-		 };
-	  }
-	  default: {
-		 return state;
-	  }
-	}
- };
-
-
+export const registerReducer = (
+  state = initialState,
+  action: ActionRegister
+): TRegisterState => {
+  switch (action.type) {
+    case GET_REGISTER: {
+      return {
+        ...state,
+        registerRequest: true,
+        registerFailed: false,
+      };
+    }
+    case GET_REGISTER_SUCCESS: {
+      return {
+        ...state,
+        email: action.email,
+        name: action.name,
+        registerRequest: false,
+      };
+    }
+    case GET_REGISTER_FAILED: {
+      return {
+        ...state,
+        registerFailed: true,
+        registerRequest: false,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
