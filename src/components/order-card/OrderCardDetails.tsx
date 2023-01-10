b5a2ee96d8543d7926bbc196cde9a6918bfcb63b
SvetaMyasoedova <so.myasoedova@gmail.com>
2023-01-10 13:20:08 +0500

    OrderCardDetails was typed

--- src/components/order-card/OrderCardDetails.tsx
+++ src/components/order-card/OrderCardDetails.tsx
@@ -1,4 +1,7 @@
-import { CurrencyIcon, FormattedDate } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  CurrencyIcon,
+  FormattedDate,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useParams } from "react-router-dom";
@@ -7,6 +10,8 @@ import { WS_PROFILE_CURRENT_ORDER } from "../../services/actions/wsProfileAction
 import { TLocationState } from "../../services/types/location";
 import IngredientDetails from "./ingredientDetails";
 import styleOrderCardDetails from "./order-card-details.module.css";
+import { TOrder } from "../../services/types/order";
+import { TIngredient } from "../../services/types/data";
 
 interface IOrderDetailsParams {
   orderId: string;
@@ -14,47 +19,49 @@ interface IOrderDetailsParams {
 
 const OrderCardDetails = () => {
   const location = useLocation<TLocationState>();
-  const { orders } = useSelector((state: any) =>{
-    if(location.pathname.includes("/profile/orders")) {
-      return state.wsProfileReducer
+  const { orders } = useSelector((state: any) => {
+    if (location.pathname.includes("/profile/orders")) {
+      return state.wsProfileReducer;
     } else {
-      return state.wsReducer
+      return state.wsReducer;
     }
   });
   const { orderId } = useParams<IOrderDetailsParams>();
   const { data } = useSelector((state: any) => state.dataReducer);
-  const currentOrder = useSelector(
-    (state: any) => {
-      if(location.pathname.includes("/profile/orders")) {
-        return state.wsProfileReducer.currentOrder
-      } else {
-        return state.wsReducer.currentOrder
-      }
+  const currentOrder = useSelector((state: any) => {
+    if (location.pathname.includes("/profile/orders")) {
+      return state.wsProfileReducer.currentOrder;
+    } else {
+      return state.wsReducer.currentOrder;
     }
-  );
- 
+  });
+
   const background = location.state && location.state.background;
   const dispatch = useDispatch();
 
-  const [counts, setCounts] = useState<any>({});
-  const [filteredArray, setFilteredArray] = useState<any>([]);
+  const [counts, setCounts] = useState<{ [key: string]: number }>({});
+  const [filteredArray, setFilteredArray] = useState<TIngredient[]>([]);
 
   useEffect(() => {
     if (orders.length !== 0 && !background) {
       dispatch({
         payload: {
-          currentOrder: orders.find((item: any) => item._id === orderId),
+          currentOrder: orders.find((item: TOrder) => item._id === orderId),
         },
-        type: location.pathname.includes("/profile/orders") ? WS_PROFILE_CURRENT_ORDER : WS_CURRENT_ORDER,
+        type: location.pathname.includes("/profile/orders")
+          ? WS_PROFILE_CURRENT_ORDER
+          : WS_CURRENT_ORDER,
       });
     }
 
     if (background && (currentOrder === null || currentOrder === undefined)) {
       dispatch({
         payload: {
-          currentOrder: orders.find((item: any) => item._id === orderId),
+          currentOrder: orders.find((item: TOrder) => item._id === orderId),
         },
-        type: location.pathname.includes("/profile/orders") ? WS_PROFILE_CURRENT_ORDER : WS_CURRENT_ORDER,
+        type: location.pathname.includes("/profile/orders")
+          ? WS_PROFILE_CURRENT_ORDER
+          : WS_CURRENT_ORDER,
       });
     }
   }, [orders]);
@@ -64,16 +71,16 @@ const OrderCardDetails = () => {
       return;
     }
 
-    const tCounts: any = {};
-    currentOrder.ingredients.forEach((id: any, index: any) => {
+    const tCounts: { [key: string]: number } = {};
+    currentOrder.ingredients.forEach((id: string, index: number) => {
       tCounts[id] = (tCounts[id] || 0) + 1;
     });
 
     const tUniqueIngredients = currentOrder.ingredients.filter(
-      (v: string, i: number, a: any) => a.indexOf(v) === i
+      (v: string, i: number, a: string[]) => a.indexOf(v) === i
     );
 
-    const tFilteredArray = data.filter((value: any) =>
+    const tFilteredArray = data.filter((value: TIngredient) =>
       tUniqueIngredients.includes(value._id)
     );
 
@@ -102,7 +109,7 @@ const OrderCardDetails = () => {
       <div className={`${styleOrderCardDetails.details}`}>
         <p className="text text_type_main-medium mb-6">Состав:</p>
 
-        {filteredArray.map((ingredient: any) => {
+        {filteredArray.map((ingredient: TIngredient) => {
           return (
             <div
               key={ingredient._id}
@@ -120,27 +127,26 @@ const OrderCardDetails = () => {
       </div>
       <div className={styleOrderCardDetails.priceData}>
         <div className="text text_type_main-default text_color_inactive">
-        <FormattedDate date={new Date(currentOrder.createdAt)} />
+          <FormattedDate date={new Date(currentOrder.createdAt)} />
+        </div>
+        <div className={styleOrderCardDetails.price}>
+          <p className="text text_type_digits-default">
+            {filteredArray.reduce(
+              (acc: number, ingredient: TIngredient, currentIndex: number) => {
+                let sum: number = 0;
+                if (currentIndex === 0) {
+                  sum = acc + ingredient.price * 2;
+                } else {
+                  sum = acc + ingredient.price;
+                }
+                return sum;
+              },
+              0
+            )}
+          </p>
+          <CurrencyIcon type="primary" />
+        </div>
       </div>
-      <div className={styleOrderCardDetails.price}>
-            <p className="text text_type_digits-default">
-              {filteredArray.reduce(
-                (acc: number, ingredient: any, currentIndex: number) => {
-                  let sum: number = 0;
-                  if (currentIndex === 0) {
-                    sum = acc + ingredient.price * 2;
-                  } else {
-                    sum = acc + ingredient.price;
-                  }
-                  return sum;
-                },
-                0
-              )}
-            </p>
-            <CurrencyIcon type="primary" />
-          </div>
-      </div>
-      
     </div>
   );
 };
