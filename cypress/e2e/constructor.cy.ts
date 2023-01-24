cb56f88e1b1e1ecd4b576906dadb4305b46ff46e
SvetaMyasoedova <so.myasoedova@gmail.com>
2023-01-24 12:39:03 +0700

    ingredient modal & close was added

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -15,5 +15,19 @@ it('should drag & drop', function () {
   cy.get('span').contains('Краторная булка N-200i (верх)');
 });
 
+it('should ingredient modal  & close', function () {
+  cy.visit('http://localhost:3000');
+  cy.get('[id="60d3b41abdacab0026a733c6"]').click();
+  cy.get('[data-test-id="ingredient-name"]').contains('Краторная булка N-200i');
+  cy.get('[data-test-id="ingredient-calories"]').contains('420');
+  cy.get('[data-test-id="ingredient-proteins"]').contains('80');
+  cy.get('[data-test-id="ingredient-fat"]').contains('24');
+  cy.get('[data-test-id="ingredient-carbohydrates"]').contains('53');
+  cy.wait(4000);
+  cy.get('[data-test-id="modal-close-icon"]').click();
+  cy.wait(1000);
+  cy.get('[data-test-id="modal-close-icon"]').should('not.exist');
+});
+
 
 
